a92e7677bb8ee448ef39e2393751cb597de2a256
Afiq Haikal <afiqhaikal571@gmail.com>
2025-07-04 11:13:18 +0800

    FIXED

--- knife_detector.py
+++ knife_detector.py
@@ -14,20 +14,12 @@ st.title("🔪 Knife Detection (Upload Image)")
 uploaded_file = st.file_uploader("Upload an image", type=['jpg', 'png', 'jpeg'])
 
 if uploaded_file is not None:
-    # Read image using PIL
     image = Image.open(uploaded_file).convert("RGB")
-
-    # Convert to numpy array (YOLOv8 needs this)
     image_np = np.array(image)
 
-    # Show original image
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Run detection
     results = model.predict(image_np)
-
-    # Plot results
     annotated = results[0].plot()
 
-    # Show annotated image
     st.image(annotated, caption="Detection Result", use_column_width=True)
